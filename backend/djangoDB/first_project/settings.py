d9a305d59da8d4daa3b80908376458e6ac8431d7
Ignatius Yap <ignatiusyap@gmail.com>
2022-03-04 05:30:21 +0800

    Update database setting

--- backend/djangoDB/first_project/settings.py
+++ backend/djangoDB/first_project/settings.py
@@ -105,8 +105,9 @@ DATABASES = {
 }
 
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=600)
+#DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
